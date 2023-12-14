ba0d48a5b82331161d5cc9f539e1ced3197e5bd6
Zach <zachkoh0871@gmail.com>
2023-12-14 19:23:56 +0000

    Got the slider to work but restarts the song when dragging the slider to the end

--- SimpleMusicPlayer/PlayerViewController.swift
+++ SimpleMusicPlayer/PlayerViewController.swift
@@ -17,10 +17,10 @@ class PlayerViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var trackName: UILabel!
     @IBOutlet var artistName: UILabel!
-
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(position)
     }
     
     override func viewDidLayoutSubviews() {
@@ -42,14 +42,31 @@ class PlayerViewController: UIViewController {
             print("Error occured loading file...")
         }
         
+        //Configuring slider
+        slider.maximumValue = Float(musicPlaying?.duration ?? 0)
+        //var Timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: Selector("updateSlider"), userInfo: nil, repeats: true)
+        
+        //Timer for the slider
+        _ = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.updateSlider), userInfo: nil, repeats: true)
         
         
         //set up user interface elements
-        
         let theImage: UIImage = UIImage(named: song.imageName)!
         imageView.image = theImage
         trackName.text = song.name
         artistName.text = song.artistName
+        
     }
 
+    
+    @IBAction func audioChangeTime(_ sender: Any) {
+        musicPlaying?.stop()
+        musicPlaying?.currentTime = TimeInterval(slider.value)
+        musicPlaying?.prepareToPlay()
+        musicPlaying?.play()
+    }
+    
+    @objc func updateSlider(){
+        slider.value = Float(musicPlaying?.currentTime ?? 0)
+    }
 }
